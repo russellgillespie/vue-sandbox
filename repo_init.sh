276fe68bd475fa54dba6bd7786c3090a37bb9264
um-russg <russg@umich.edu>
2025-09-29 13:48:07 -0400

    Initial Vue Sandbox commit

--- repo_init.sh
+++
@@ -1,4 +0,0 @@
-npm pkg set homepage="https://russellgillespie.github.io/vue-sandbox"
-npm install --save-dev gh-pages
-npm pkg set scripts.predeploy="npm run build"
-npm pkg set scripts.deploy="gh-pages -d build"
